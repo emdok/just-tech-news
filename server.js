d25e705e11093e76e3fee52313e8cfae4aabbcce
Emily Dokken <dokkenemily@gmail.com>
2022-02-09 22:56:15 -0800

    add helpers and partials

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const helpers = require('./utils/helpers');
 const session = require('express-session');
 const path = require('path');
 const express = require('express');
@@ -5,7 +6,7 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const SequlizeStore = require('connect-session-sequelize')(session.Store);
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const { listen } = require('express/lib/application');
 
 const sess = {
